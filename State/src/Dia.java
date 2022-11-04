2b748af4cc6cec6e5ef54490e2c5adf48ddaf0ec
Rodrigo Nathan Oliveira <rodrigonathan4101@gmail.com>
2022-11-04 18:20:30 -0300

    trab bruto

---
+++ State/src/Dia.java
@@ -0,0 +1,27 @@
+public class Dia {
+
+    protected EstadoDia estado;
+
+    public Dia() {
+        estado = new Manha();
+    }
+
+  
+    public void anoitecer() {
+        estado = estado.anoitecer();
+    }
+
+  
+    public void amanhecer() {
+        estado = estado.amanhecer();
+     
+    }
+
+
+    public void atardecer() {
+        estado = estado.atardecer();
+    }
+
+    
+    
+}
---
+++ State/src/EstadoDia.java
@@ -0,0 +1,9 @@
+
+public interface EstadoDia {
+
+    EstadoDia anoitecer();
+    EstadoDia amanhecer();
+    EstadoDia atardecer();
+
+
+}
